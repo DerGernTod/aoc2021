dce7351eb7d21f1b549886c7c7af8baa1b9ae7d2
Gernot Raudner <git@gernotraudner.at>
2021-12-20 23:35:44 +0100

    day 18 part 1 incomplete

--- src/day_18.rs
+++ src/day_18.rs
@@ -1,51 +1,51 @@
-use std::{fs, rc::Rc};
+use std::{fs, rc::Rc, collections::HashMap};
 
 #[derive(PartialEq, Eq, Debug)]
 struct NumberInfo {
     value: u32,
     depth: u32,
-    parent: Option<Rc<PairInfo>>
+    parent: usize
 }
 
 #[derive(PartialEq, Eq, Debug)]
 struct PairInfo {
-    left: Rc<NumberEntry>,
-    right: Rc<NumberEntry>,
+    left: usize,
+    right: usize,
     depth: u32,
-    parent: Option<Rc<PairInfo>>
+    parent: usize
 }
 
 impl NumberInfo {
-    fn new(value: u32, depth: u32) -> NumberInfo {
+    fn new(value: u32, depth: u32, parent_id: usize) -> NumberInfo {
         NumberInfo {
             value,
             depth,
-            parent: None
+            parent: parent_id
         }
     }
-    fn set_parent(&mut self, parent: Rc<PairInfo>) {
-        self.parent = Some(parent);
+    fn set_parent(&mut self, parent: usize) {
+        self.parent = parent;
     }
 }
 
 impl PairInfo {
-    fn new(left: Rc<NumberEntry>, right: Rc<NumberEntry>, depth: u32) -> PairInfo {
+    fn new(left: usize, right: usize, depth: u32, parent_id: usize) -> PairInfo {
         PairInfo {
             left,
             right,
             depth,
-            parent: None
+            parent: parent_id
         }
     }
-    fn set_parent(&mut self, parent: Rc<PairInfo>) {
-        self.parent = Some(parent);
+    fn set_parent(&mut self, parent: usize) {
+        self.parent = parent;
     }
 }
 
 #[derive(PartialEq, Eq, Debug)]
 enum NumberEntry {
-    Literal(Rc<NumberInfo>),
-    Pair(Rc<PairInfo>),
+    Literal(NumberInfo),
+    Pair(PairInfo),
     None
 }
 
@@ -65,41 +65,38 @@ fn add_fishnumbers_from_path(path: &str)  {
         .map(|str| str.chars().into_iter().collect())
         .collect();
     let mut cursor = 0;
-    let result_pair = create_pair(0, &mut cursor, str.get(0).unwrap());
-    println!("parsed results: {:?}", result_pair);
+    let mut lookup = HashMap::new();
+    create_pair(0, &mut cursor, str.get(0).unwrap(), &mut lookup, 0);
+    println!("parsed results: {:?}", lookup);
 }
 
-fn create_pair(depth: u32, cursor: &mut usize, cur_operation: &Vec<char>) -> Rc<PairInfo> {
+fn create_literal(depth: u32, cursor: &mut usize, value: u32, lookup: &mut HashMap<usize, NumberEntry>, parent_id: usize) -> usize {
+    let entry = NumberEntry::Literal(NumberInfo::new(value, depth, parent_id));
+    lookup.entry(*cursor).or_insert(entry);
+    *cursor
+}
+
+fn create_pair(depth: u32, cursor: &mut usize, cur_operation: &Vec<char>, lookup: &mut HashMap<usize, NumberEntry>, parent_id: usize) -> usize {
+    let pair_id = *cursor;
     *cursor += 1;
     let c = cur_operation.get(*cursor).unwrap();
-    let left = Rc::new(match c {
-        '[' => NumberEntry::Pair(create_pair(depth + 1, cursor, cur_operation)),
-        '0'..='9' => NumberEntry::Literal(Rc::new(NumberInfo::new(c.to_digit(10).unwrap(), depth))),
+    let left = match c {
+        '[' => create_pair(depth + 1, cursor, cur_operation, lookup, pair_id),
+        '0'..='9' => create_literal(depth + 1, cursor, c.to_digit(10).unwrap(), lookup, pair_id),
         x => panic!("We got something other than '[' or 0-9 after a '[' at index {}: {:?}", *cursor, x)
-    });
+    };
     // we must be at ] or number now, so skip this and a ,
     *cursor += 2;
     let c = cur_operation.get(*cursor).unwrap();
-    let right = Rc::new(match c {
-        '[' => NumberEntry::Pair(create_pair(depth + 1, cursor, cur_operation)),
-        '0'..='9' => NumberEntry::Literal(Rc::new(NumberInfo::new(c.to_digit(10).unwrap(), depth))),
+    let right = match c {
+        '[' => create_pair(depth + 1, cursor, cur_operation, lookup, pair_id),
+        '0'..='9' => create_literal(depth + 1, cursor, c.to_digit(10).unwrap(), lookup, pair_id),
         x => panic!("We got something other than '[' or 0-9 after a ',' at index {}: {:?}", *cursor, x)
-    });
+    };
     // we must be at a ']' now
     *cursor += 1;
-    let pair = PairInfo::new(Rc::clone(&left), Rc::clone(&right), depth + 1);
-    let pair_rc = Rc::new(pair);
-    match Rc::clone(&left).as_ref() {
-        NumberEntry::Literal(mut x) => x.set_parent(Rc::clone(&pair_rc)),
-        NumberEntry::Pair(mut x) => x.set_parent(Rc::clone(&pair_rc)),
-        _ => ()
-    };
-    match right.as_ref() {
-        NumberEntry::Literal(mut x) => x.set_parent(Rc::clone(&pair_rc)),
-        NumberEntry::Pair(mut x) => x.set_parent(Rc::clone(&pair_rc)),
-        _ => ()
-    };
-    pair_rc
+    lookup.entry(pair_id).or_insert(NumberEntry::Pair(PairInfo::new(left, right, depth + 1, parent_id)));
+    pair_id
 }
 
 #[cfg(test)]
